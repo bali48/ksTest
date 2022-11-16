0e7a2bf2552928b922b2231a19f652cc263dac8d
bali48 <m.bilal294@outlook.com>
2022-11-16 20:10:07 +0500

    test completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Draggable from "react-draggable";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Draggable bounds='.App'>
+        <div className='box'>
+          <div>Drag me</div>
+        </div>
+      </Draggable>
     </div>
   );
 }
